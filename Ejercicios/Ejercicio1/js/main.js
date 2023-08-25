b1ae2d9eb0f4b9cff6cc43e762486aac26be3e7b
AngelLaines <angelmoreno755@gmail.com>
2023-08-25 12:27:51 -0700

    Ej1 esqueleto de la pagina

---
+++ Ejercicios/Ejercicio1/js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollToTopButton = document.getElementById('btn-ir-arriba');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+
+    scrollToTopButton.addEventListener('click', () => {
+        window.scrollTo({ 
+            top: 0,
+            behavior: "smooth" 
+        })
+    });
+});
